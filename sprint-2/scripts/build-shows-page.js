17812a4c4f2a838cfe90b76f1331639f90e51b56
Phoebe Lo <phoebeyclo@gmail.com>
2020-09-06 02:42:28 -0700

    Styling shows page

---
+++ sprint-2/scripts/build-shows-page.js
@@ -0,0 +1,15 @@
+/* <div class="shows__info shows__row--divider">
+        <div class="shows__container">
+            <div class="shows__subtitle shows__subtitle--info">Date</div>
+            <div class="shows__date">Mon Dec 17 2018</div>
+        </div>
+        <div class="shows__container">
+            <div class="shows__subtitle shows__subtitle--info">venue</div>
+            <div class="shows__venue">Ronald Lane</div>
+        </div>
+        <div class="shows__container">
+            <div class="shows__subtitle shows__subtitle--info">location</div>
+            <div class="shows__location">location</div>
+        </div>
+        <button class="shows__buy-btn">buy ticket</button>
+    </div> */
